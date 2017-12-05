a694765c9fd778f61d3afd9340ac59aaea878022
Boskin <boskin.erkocevic@gmail.com>
2017-12-04 22:54:15 -0800

    Finished the i-type case for instructions.

--- id_decoder.v
+++ id_decoder.v
@@ -4,10 +4,10 @@
 
 module id_decoder(
   instr,
-    
+
   alu_op,
   imm,
-  
+
   alu_a_src,
   alu_b_src,
   dest_src
@@ -18,25 +18,25 @@ module id_decoder(
   parameter WORD_W = `WORD_W;
 
   input [INSTR_W - 1:0] instr;
-  
+
   output reg [ALU_OP_W - 1:0] alu_op;
   output reg [WORD_W - 1:0] imm;
-  
+
   output reg [`ALU_SRC_A_W - 1:0] alu_a_src;
   output reg [`ALU_SRC_B_W - 1:0] alu_b_src;
   output reg [`DEST_SRC_W - 1:0] dest_src;
-  
+
   wire [6:0] opcode = `OPCODE(instr);
   wire [2:0] funct3 = `FUNCT3(instr);
   wire [6:0] funct7 = `FUNCT7(instr);
-  
+
   wire [16:0] opcode_rtype = `OPCODE_COMPLETE_RTYPE(instr);
   wire [16:0] opcode_itype = `OPCODE_COMPLETE_ITYPE(instr);
-  
+
   reg [16:0] opcode_complete;
-  
+
   wire [11:0] itype_imm12 = `ITYPE_IMM12(instr);
-  
+
   always@(*) begin
     case(opcode)
       `OPCODE_RTYPE: opcode_complete = opcode_rtype;
@@ -44,34 +44,34 @@ module id_decoder(
       default: opcode_complete = 0;
     endcase
   end
-  
+
   always@(*) begin
     case(opcode)
       `OPCODE_RTYPE: begin
         imm = 0;
-        
+
         alu_a_src = `ALU_SRC_A_XPR;
         alu_b_src = `ALU_SRC_B_XPR;
         dest_src = `DEST_SRC_ALU;
       end
-      
-      `OPCODE_ITYPE: begin 
+
+      `OPCODE_ITYPE: begin
         imm = $signed(itype_imm12);
-        
+
         alu_a_src = `ALU_SRC_A_XPR;
         alu_b_src = `ALU_SRC_B_IMM;
         dest_src = `DEST_SRC_ALU;
       end
       default: begin
         imm = 0;
-        
+
         alu_a_src = `ALU_SRC_A_XPR;
         alu_b_src = `ALU_SRC_B_XPR;
         dest_src = `DEST_SRC_NONE;
       end
     endcase
   end
-  
+
   // ALU OP select
   always@(*) begin
     case(opcode_complete)
