efdc670013d5ed602d38a33ea30ea41d67d99e67
Boskin <boskin.erkocevic@gmail.com>
2018-02-01 23:29:23 -0800

    Added a test case where an attempt to write data to register 0 occurs.

--- tb/tb_register_file.v
+++ tb/tb_register_file.v
@@ -23,7 +23,68 @@ module tb_register_file;
   reg wr_en;
   reg [REG_IDX_W - 1:0] wr_reg;
   reg [REG_W - 1:0] wr_data;
-  reg [REG_W - 1:0] data_delay;
+  reg [REG_W - 1:0] wr_data_check;
+  reg [REG_W - 1:0] rd_data_tmp;
+
+  integer i;
+  initial begin
+    $dumpfile("tb_register_file.vcd");
+    $dumpvars;
+    
+    clk = 0;
+    aresetn = 0;
+
+    rd_reg_a = 0;
+    rd_reg_b = 0;
+
+    wr_en = 0;
+    wr_reg = 0;
+    wr_data = 0;
+  
+    #RESET_DURATION;
+    aresetn = 1;
+
+    wr_en = 1;
+    wr_reg = 0;
+    wr_data = 1;
+    rd_reg_a = 0;
+
+    #CLK_PERIOD;
+
+    if(rd_data_a != 0) begin
+      $display("Assertion failed! Wrote something to register 0!");
+      $finish;
+    end
+
+    #CLK_PERIOD;
+
+    wr_en = 1;
+
+    // Write random data to each register and read
+    for(i = 1; i < REG_COUNT; i = i + 1) begin
+      wr_reg = i;
+      rd_reg_a = i;
+      
+      wr_data = $urandom;
+      wr_data_check = wr_data;
+
+      #CLK_PERIOD;
+
+      // Check if the data matches
+      if(rd_data_tmp != wr_data_check) begin
+        $display("Error! Read/write mismatch!");
+        $finish;
+      end
+    end
+    $display("All assertions passed!");
+    $finish;
+  end
+
+  // Clock generator
+  always begin
+    #(CLK_PERIOD / 2);
+    clk = ~clk;
+  end
 
   register_file#(
     .REG_W(REG_W),
@@ -42,47 +103,4 @@ module tb_register_file;
     .wr_reg(wr_reg),
     .wr_data(wr_data)
   );
-
-  integer i;
-  initial begin
-    $dumpfile("tb_register_file.vcd");
-    $dumpvars;
-    
-    clk = 0;
-    aresetn = 0;
-
-    rd_reg_a = 0;
-    rd_reg_b = 0;
-
-    wr_en = 0;
-    wr_reg = 0;
-    wr_data = 0;
-
-    data_delay = 0;
-  
-    #RESET_DURATION
-    aresetn = 1;
-
-    for(i = 1; i < REG_COUNT; i = i + 1) begin
-      wr_en = 1;
-      
-      wr_reg = i;
-      rd_reg_a = i;
-      
-      wr_data = $urandom;
-
-      #CLK_PERIOD;
-    end
-    $finish;
-  end
-
-  
-
-  always begin
-    clk = 0;
-    #(CLK_PERIOD / 2);
-    clk = 1;
-    #(CLK_PERIOD / 2);
-  end
-
 endmodule
