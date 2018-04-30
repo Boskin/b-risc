a08fe9870a6e912649428c351ef2784063a7a51a
Boskin <boskin.erkocevic@gmail.com>
2018-04-30 01:10:22 -0600

    Added a few r-type instructions.

--- tools/compiler/main.cpp
+++ tools/compiler/main.cpp
@@ -4,13 +4,21 @@
 #include "r_instruction.hpp"
 #include "i_instruction.hpp"
 
+#define OPCODE(funct7, funct3, opcode) ((uint32_t)((funct7 << 10) | (funct3 << 7) | opcode))
+
 void load_instruction(std::vector<std::string>& code, const char* file);
 
 int main() {
     std::unordered_map<std::string, Instruction_Handler_Base*> handlers;
 
-    handlers["add"] = new Instruction_Handler_R(0x33);
-    handlers["addi"] = new Instruction_Handler_I(0x13);
+    handlers["add"] = new Instruction_Handler_R(OPCODE(0, 0, 0x33));
+    handlers["sub"] = new Instruction_Handler_R(OPCODE(0x20, 0, 0x33));
+    handlers["sll"] = new Instruction_Handler_R(OPCODE(0, 0x1, 0x33));
+    handlers["slt"] = new Instruction_Handler_R(OPCODE(0, 0x2, 0x33));
+    handlers["sltu"] = new Instruction_Handler_R(OPCODE(0, 0x3, 0x33));
+    handlers["xor"] = new Instruction_Handler_R(OPCODE(0, 0x4, 0x33));
+    handlers["srl"] = new Instruction_Handler_R(OPCODE(0, 0x5, 0x33));
+    handlers["addi"] = new Instruction_Handler_I(OPCODE(0, 0, 0x13));
 
     std::cout << "Welcome to the compiler that's still under construction!\n";
 
@@ -45,13 +53,10 @@ int main() {
 void load_instruction(std::vector<std::string>& code, const char* file) {
     std::ifstream in(file, std::ios::in);
 
-    while(!in.bad() && !in.eof()) {
-        char instr[1024];
-        in.getline(instr, 1023);
+    char instr[1024];
+    while(in.getline(instr, 1023)) {
         code.push_back(std::string(instr));
     }
-
-    code.pop_back();
 
     in.close();
 }
