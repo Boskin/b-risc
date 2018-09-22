4f3329c04005c7fd149a6e640c9e58c8d8c8787d
Boskin Erkocevic <boskin.erkocevic@gmail.com>
2018-09-22 12:04:20 -0600

    Added the timer module which utilizes banks of read/write and readonly registers for configuration.

--- memory_peripheral/timer.v
+++ memory_peripheral/timer.v
@@ -1,4 +1,5 @@
 `include "config.vh"
+`include "mem_codes.vh"
 
 module timer(
   clk,
@@ -13,8 +14,11 @@ module timer(
   o_res_code
 );
   parameter [`ADDR_W - 1:0] ADDR_START = 0;
-  localparam ADDR_COUNT = 8;
-  localparam ADDR_END = ADDR_START + ADDR_COUNT;
+
+  localparam RW_ADDR_START = ADDR_START;
+  localparam RW_ADDR_COUNT = 3;
+  localparam RO_ADDR_START = RW_ADDR_START + RW_ADDR_COUNT;
+  localparam RO_ADDR_COUNT = 2;
 
   input clk;
   input aresetn;
@@ -27,32 +31,103 @@ module timer(
   output reg [`WORD_W - 1:0] o_res_rd_data;
   output reg o_res_code;
 
-  wire [`WORD_W - 3:0] s_addr_aligned;
-  assign s_addr_aligned = i_req_addr[`WORD_W - 1:2];
+  // Reqd/write registers
+  // Enables the counter
+  wire en;
+  // If 1, the counter will count down
+  wire backward;
+  // If 1, the counter will stay at load_val
+  wire load;
+  /* If enabled, when the counter hits the threshold, it will load the
+   * load_val */
+  wire threshold_en;
+  wire [`WORD_W - 1:0] threshold;
+  wire [`WORD_W - 1:0] load_val;
 
-  wire [1:0] s_offset;
-  assign s_offset = i_req_addr[1:0];
+  wire [RW_ADDR_COUNT * `WORD_W - 1:0] s_readwrite_data;
+  // Concatenate the data in this fashion for reads/writes from the processor
+  assign s_readwrite_data = {
+    load_val,
+    threshold,
+    {(`WORD_W - 4){1'b0}}, en, backward, load, threshold_en
+  };
 
-  reg [`WORD_W - 1:0] ctrl_reg [ADDR_COUNT - 1:0];
+  // Readonly registers
+  reg [`WORD_W - 1:0] count;
+  // High if the count equals the threshold
+  wire threshold_trigger;
 
+  wire [RO_ADDR_COUNT * `WORD_W - 1:0] s_readonly_data;
+  assign s_readonly_data = {
+    count,
+    {(`WORD_W - 1){1'b0}}, threshold_trigger
+  };
+
+  wire [`WORD_W - 1:0] s_rw_rd_data;
+  wire [`MEM_CODE_W - 1:0] s_rw_code;
+
+  wire [`WORD_W - 1:0] s_ro_rd_data;
+  wire [`MEM_CODE_W - 1:0] s_ro_code;
+
+  assign threshold_trigger = count == threshold && aresetn == 1;
   always@(posedge clk, negedge aresetn) begin
-    if(aresetn == 0) begin
-    end else begin
-      if(i_req_addr >= ADDR_START && i_req_addr < ADDR_END) begin
-        if(i_req_count == `MEM_CODE_NONE) begin
-          o_res_code <= `MEM_CODE_INVALID;
-        end else if(i_req_count == `MEM_COUNT_WORD && s_offset != 0 ||
-          i_req_count == `MEM_COUNT_HALF && s_offset[0] == 1) begin
-        
-          o_res_code <= `MEM_CODE_MISALIGNED;
-        end else if(i_req_wr_en == 1 && i_req_addr != ADDR_END - `WORD_W) begin
-          case(i_req_count)
-            `MEM_CODE_WORD:
-          endcase
-        end
+    if (aresetn == 0) begin
+      count <= 0;
+    end
+    if (en == 1) begin
+      if (backward == 0) begin
+        count <= count + 1;
+      end else begin
+        count <= count - 1;
       end
+    end
+  end
+
+  // Bank of read/write registers
+  readwrite_registers#(
+    .ADDR_START(RW_ADDR_START),
+    .ADDR_COUNT(RW_ADDR_COUNT)
+  ) rw_reg(
+    .clk(clk),
+    .aresetn(aresetn),
+
+    .i_req_addr(i_req_addr),
+    .i_req_wr_data(i_req_wr_data),
+    .i_req_wr_en(i_req_wr_en),
+    .i_req_count(i_req_count),
+
+    .o_res_rd_data(s_rw_rd_data),
+    .o_res_code(s_rw_code),
+
+    .o_exposed_mem(s_readwrite_data)
+  );
+
+  // Bank of readonly registers
+  readonly_registers#(
+    .ADDR_START(RO_ADDR_START),
+    .ADDR_COUNT(RO_ADDR_COUNT)
+  ) ro_reg(
+    .clk(clk),
+    .aresetn(aresetn),
+
+    .i_registers(s_readonly_data),
+
+    .i_req_addr(i_req_addr),
+    .i_req_count(i_req_count),
+
+    .o_res_rd_data(s_ro_rd_data),
+    .o_res_code(s_ro_code)
+  );
+
+  /* Mux the responses from the different register modules by looking at the
+   * request address */
+  always@(*) begin: output_mux
+    if(i_req_addr >= RW_ADDR_START && i_req_addr < RO_ADDR_START) begin
+      o_res_rd_data = s_rw_rd_data;
+      o_res_code = s_rw_code;
     end else begin
-      o_res_code <= `MEM_CODE_OUT_OF_BOUNDS;
+      o_res_rd_data = s_ro_rd_data;
+      o_res_code = s_ro_code;
     end
   end
 endmodule
