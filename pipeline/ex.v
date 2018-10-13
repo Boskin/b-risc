389800c0b13132cc3bdcad1010d7fc36f540af2d
Boskin Erkocevic <boskin.erkocevic@gmail.com>
2018-10-13 14:02:11 -0700

    Added signals for branching and forwarding.

--- pipeline/ex.v
+++ pipeline/ex.v
@@ -1,4 +1,5 @@
 `include "config.vh"
+`include "opcodes.vh"
 `include "mem_codes.vh"
 `include "alu_op.vh"
 
@@ -12,6 +13,9 @@ module ex(
 
   i_alu_op,
 
+  i_alu_reg_a,
+  i_alu_reg_b,
+
   i_alu_data_a,
   i_alu_data_b,
   i_imm,
@@ -21,8 +25,13 @@ module ex(
   i_dest_src,
   i_dest_reg,
 
+  i_branch_op,
+
   o_pc,
   o_instr,
+
+  o_alu_reg_a,
+  o_alu_reg_b,
 
   o_dest_src,
   o_dest_reg,
@@ -32,7 +41,10 @@ module ex(
   o_mem_req_addr,
   o_mem_req_wr_data,
   o_mem_req_wr_en,
-  o_mem_req_count
+  o_mem_req_count,
+
+  o_branch,
+  o_branch_addr
 );
 
   input clk;
@@ -44,6 +56,9 @@ module ex(
 
   input [`ALU_OP_W - 1:0] i_alu_op;
 
+  input [`REG_IDX_W - 1:0] i_alu_reg_a;
+  input [`REG_IDX_W - 1:0] i_alu_reg_b;
+
   input [`WORD_W - 1:0] i_alu_data_a;
   input [`WORD_W - 1:0] i_alu_data_b;
   input [`WORD_W - 1:0] i_imm;
@@ -53,10 +68,15 @@ module ex(
   input [`DEST_SRC_W - 1:0] i_dest_src;
   input [`REG_IDX_W - 1:0] i_dest_reg;
 
+  input i_branch_op;
+
   reg [`ADDR_W - 1:0] r_pc;
   reg [`INSTR_W - 1:0] r_instr;
 
   reg [`ALU_OP_W - 1:0] r_alu_op;
+
+  reg [`REG_IDX_W - 1:0] r_alu_reg_a;
+  reg [`REG_IDX_W - 1:0] r_alu_reg_b;
 
   reg [`WORD_W - 1:0] r_alu_data_a;
   reg [`WORD_W - 1:0] r_alu_data_b;
@@ -67,11 +87,16 @@ module ex(
   reg [`DEST_SRC_W - 1:0] r_dest_src;
   reg [`REG_IDX_W - 1:0] r_dest_reg;
 
+  reg r_branch_op;
+
   wire [`WORD_W - 1:0] s_alu_eval;
   wire s_alu_zero;
 
   output [`ADDR_W - 1:0] o_pc;
   output [`INSTR_W - 1:0] o_instr;
+
+  output [`REG_IDX_W - 1:0] o_alu_reg_a;
+  output [`REG_IDX_W - 1:0] o_alu_reg_b;
 
   output [`DEST_SRC_W - 1:0] o_dest_src;
   output [`REG_IDX_W - 1:0] o_dest_reg;
@@ -83,14 +108,23 @@ module ex(
   output reg o_mem_req_wr_en;
   output reg [`MEM_COUNT_W - 1:0] o_mem_req_count;
 
+  output o_branch;
+  output [`ADDR_W - 1:0] o_branch_addr;
+
   // Outputs
   assign o_pc = r_pc;
   assign o_instr = r_instr;
+
+  assign o_alu_reg_a = r_alu_reg_a;
+  assign o_alu_reg_b = r_alu_reg_b;
 
   assign o_dest_src = r_dest_src;
   assign o_dest_reg = r_dest_reg;
 
   assign o_alu_eval = s_alu_eval;
+
+  assign o_branch = s_alu_zero & r_branch_op;
+  assign o_branch_addr = r_pc + r_imm;
 
   always@(posedge clk) begin
     if(clr == 1) begin
@@ -98,6 +132,9 @@ module ex(
       r_instr <= 0;
 
       r_alu_op <= `ALU_ADD;
+
+      r_alu_reg_a <= 0;
+      r_alu_reg_b <= 0;
 
       r_alu_data_a <= 0;
       r_alu_data_b <= 0;
@@ -107,6 +144,8 @@ module ex(
 
       r_dest_src <= `DEST_SRC_NONE;
       r_dest_reg <= 0;
+
+      r_branch_op <= 0;
     end else if(stall == 0) begin
       r_pc <= i_pc;
       r_instr <= i_instr;
@@ -115,12 +154,17 @@ module ex(
 
       r_mem_op <= i_mem_op;
 
+      r_alu_reg_a <= i_alu_reg_a;
+      r_alu_reg_b <= i_alu_reg_b;
+
       r_alu_data_a <= i_alu_data_a;
       r_alu_data_b <= i_alu_data_b;
       r_imm <= i_imm;
 
       r_dest_src <= i_dest_src;
       r_dest_reg <= i_dest_reg;
+
+      r_branch_op <= i_branch_op;
     end
   end
 
