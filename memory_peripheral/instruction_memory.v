eae3149cf05311e20b42b3f59922f989415a39de
Boskin Erkocevic <boskin.erkocevic@gmail.com>
2018-10-22 00:26:30 -0600

    Added a parametrized option to dump the contents of the instruction memory. This is off by default.

--- memory_peripheral/instruction_memory.v
+++ memory_peripheral/instruction_memory.v
@@ -17,6 +17,8 @@ module instruction_memory(
 
   parameter INSTR_MAX = 100;
   parameter INSTR_FILE = "instr.txt";
+  parameter DUMP_INSTR = 0;
+  parameter DUMP_FILE = "a.vcd";
 
   input clk;
 
@@ -31,6 +33,15 @@ module instruction_memory(
   reg [`INSTR_W - 1:0] r_mem [0:INSTR_MAX - 1];
 
   initial begin
+  end
+
+  initial begin
+    if(DUMP_INSTR == 1) begin
+      $dumpfile(DUMP_FILE);
+      for(i = 0; i < INSTR_MAX; i = i + 1) begin
+        $dumpvars(0, r_mem[i]);
+      end
+    end
     instr_file = $fopen(INSTR_FILE, "r");
     if(instr_file == 0) begin
       $display("Error! Couldn't open the input instruction file!");
